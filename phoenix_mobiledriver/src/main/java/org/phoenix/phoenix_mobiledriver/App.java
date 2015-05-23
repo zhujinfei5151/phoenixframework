35120f1ab3a1c7a615a3b5543d0f89bf74124e0a
mengfeiyang <mengfeiyang@mfy>
2015-05-23 20:58:31 +0800

    phoenix_mobiledriver 1.0

---
+++ phoenix_mobiledriver/src/main/java/org/phoenix/phoenix_mobiledriver/App.java
@@ -0,0 +1,13 @@
+package org.phoenix.phoenix_mobiledriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
